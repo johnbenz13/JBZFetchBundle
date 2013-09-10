7b85009e3ac9890d9d79b88c1dc20f3267353a8e
Jonathan Bensaid <john@bensaidj.com>
2013-09-10 15:45:35 +0300

    Add service jbz_fetch

---
+++ JBZFetchBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace JBZ\FetchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Symfony Bundle that integrates Fetch library
+ * Fetch is an Imap and POP library for php
+ *
+ * @author Jonathan Bensaid <john@bensaidj.com>
+ */
+class JBZFetchBundle extends Bundle
+{
+
+}
